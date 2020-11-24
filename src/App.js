0e30c0d82edee2df26070b0ba9e3a5fa22ad0cd4
Junmannn <junmannnyoon@gmail.com>
2020-11-25 03:15:21 +0900

    App.js -> 1. react의 요소들은 전부 umipue 해야 한다 (id부여), 2. rendering. 3. props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react";
 
-function App() {
-  return <div> Hello Mofs </div>;
+
+
+// object의 list를 가졍는거 볼거야. 오직 javascript의 object들만.
+// function component, food component를 동적으로 rendering 할 것이다.
+const foodILike = [
+  {
+    id:1,
+    name: "Kimchi",
+    image:
+    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    id:2,
+    name: "Samgyeopsal",
+    image:
+    "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    id:3,
+    name: "Bibimbap",
+    image:
+    "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    id:4,
+    name: "Doncasu",
+    image:
+    "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    id:5,
+    name: "Kimbap",
+    image:
+    "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
+// react의 모든 요소들은 다르게 보일 필요가 있어. 다 unique 해야 된다.
+
+// application 에서 food component로 정보를 보내고 그런 다음에 food component에서 그 정보를 어떻게 사용하지에 대해서 한다
+function Food({name, picture}) {
+  return(
+    <div>
+      <h2> Junmannn Like {name}</h2>
+      <img src={picture} alt = {name} />
+    </div>
+  );
 }
+
+// component 안에 넣게 되는 것, 그걸 props라고 한다. 그 props가 Food의 첫 argument로 들어가는거지
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name = {dish.name} picture={dish.image} />
+        ))}
+    </div>
+  );
+}
+/* 
+  map은 array 의 각 item 에서 function을 실행하는 array를 가지는 javascript finction이며 
+  그 function의 result를 갖는 array를 나에게 준다
+*/
+// 누군가가 food component로 정보를 보내려고 하면, react는 이 모든 속성을 가져올 것이다
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));  
+// React는 component 를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
+// JavaScript와 HTML 사이의 이러한 조합을 jsx 라고 부른다. -> react에서 나온 매우 custom한 유일한 개념이다.
 
-// git config 확인
+// React application은 하나의 component 만을 redering 해야 한다. 그 component가 저 위의 <APP />이고, 저기가 <APP /> <Potato /> 이런 식으로 1개가 아니면 안된다는 소리야
