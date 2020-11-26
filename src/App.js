9285c9f8ec36fec5326235e5a836c27ae01053b1
Junmannn <junmannnyoon@gmail.com>
2020-11-27 00:26:26 +0900

    component의 생명주기에 대해서 알아보았다. componentDidMount, componentDidUpdate, render에 대해서 알게 되었다.다음은 fetch에 대해서 알아볼까 한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 // 항상 기억 React는 자동적으로 class component 의 render method를 자동으로 실행한다.
-class App extends React.Component{
+class App extends React.Component {
+  // constructor() {
+  //   console.log("hello");
+  // }
+
   // React component는 render method를 가지고 있지만, extends from 을 했으니까 render method가 있다.
-  
   state = {
     // state는 object이고 component의 data를 넣을 공간이 있고 이 데이터는 변한다.
     count: 0
@@ -13,21 +15,36 @@ class App extends React.Component{
 
   add = () => {
     console.log("add");
+    this.setState(current => ({ count: this.state.count + 1 }));
   };
 
   minus = () => {
     console.log("minus");
+    this.setState(current => ({ count: this.state.count - 1 }));
   };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount(){
+    console.log("Goodbye, cruel world");
+  }
 
   // <button onClick={this.add}> Add </button> 
   // this.add() 하면 즉시 나타나는 것이고 this.add를 해야 클릭을 하였을 때에만 되는 것이다.
-  render(){
+  render() {
+    console.log("I am rendering");
     return (
       <div>
         <h1>The number is: {this.state.count} </h1>
         <button onClick={this.add}> Add </button>
         <button onClick={this.minus}> Minus </button>
-      </div>    
+      </div>
     );
   }
 }
